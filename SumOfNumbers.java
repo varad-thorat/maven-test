60daa206a1c43645038f141bf412afa9d121541e
shreyashkadam <shreyash.kadam10@gmail.com>
2022-11-03 09:04:53 +0530

    fghgfuh

---
+++ SumOfNumbers.java
@@ -0,0 +1,7 @@
+public class SumOfNumbers {
+    public static void main(String args[]) {
+        int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println("The sum of two numbers x and y: " + sum);
+    }
+
+}
